9f4c32783ba4baa2e197153139752ef80df43b07
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-05 17:28:30 -0400

    Update browserstack_techchallenge2.py

--- browserstack_techchallenge2.py
+++ browserstack_techchallenge2.py
@@ -62,6 +62,10 @@ def tech_challenge(browser):
         driver.maximize_window()
         login_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "sign-in-link")))
         login_button.click()
+        mobile_menu = driver.find_element_by_class("primary-menu-toggle")
+        if mobile_menu.isDisplayed():
+            mobile_menu.click()
+            login_button.click()
 
         # Login using your trial credentials
         user_input = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "user_email_login")))
@@ -71,16 +75,27 @@ def tech_challenge(browser):
         pass_input.send_keys(Keys.RETURN)
 
         # 2. Make sure that the homepage includes a link to invite users and retrieve the link’s URL  
-        invite_link = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "invite-link"))).click() # Wait for the login to complete and the homepage to load
+        invite_link = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "invite-link"))) # Wait for the login to complete and the homepage to load
         assert invite_link.is_displayed(), "Invite user link not found on the homepage" # No invite link found in homepage when logged in
-        invite_url = invite_link.get_attribute("href")
+        if mobile_menu.isDisplayed():
+            mobile_menu.click()
+            invite_link.click()
+        invite_page = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "manage-users__invite-copyLink-text")))
+        invite_url = invite_link.get_attribute("innerHTML")
         print("URL to invite users:", invite_url)
+        
 
         # 3. Log out of BrowserStack
         user_account = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, "account-menu-toggle"))) # Wait for the dropdown menu to open
         user_account.click()
-        logout_button = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, "sign_out_link"))).click()
+        logout_button = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, "sign_out_link")))
         logout_button.click()
+        if mobile_menu.isDisplayed():
+            mobile_menu.click()
+            logout_button.click()
+            
+        # For marking test as passed
+        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Test passed!"}}')
     
   except NoSuchElementException as err:
         message = "Exception: " + str(err.__class__) + str(err.msg)
@@ -91,9 +106,6 @@ def tech_challenge(browser):
         driver.execute_script(
             'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
         
-  # For marking test as passed
-  driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Test passed"}}')
-    
   # Close the browser
   driver.quit()
 for browser in browsers:
