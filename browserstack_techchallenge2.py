013ce6620ef0d4cb60b83de9567729efb0b92cc7
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-06 19:39:29 -0400

    Update browserstack_techchallenge2.py

--- browserstack_techchallenge2.py
+++ browserstack_techchallenge2.py
@@ -82,7 +82,8 @@ def tech_challenge(browser):
                 pass_input.send_keys(Keys.RETURN)
                 
                 # 2. Make sure that the homepage includes a link to invite users and retrieve the link’s URL  
-                driver.find_element(By.CLASS_NAME, "collapse-toggle").click()
+                menu_toggle = driver.find_element(By.CLASS_NAME, "collapse-toggle")
+                menu_toggle.click()
                 invite_link = find_element(By.LINK_TEXT, "Invite team")
                 invite_link.click()
                 invite_page = driver.find_element(By.CLASS_NAME, "manage-users__invite-copyLink-text")
@@ -90,7 +91,8 @@ def tech_challenge(browser):
                 print("URL to invite users:", invite_url)
    
                 # 3. Log out of BrowserStack
-                driver.find_element(By.CLASS_NAME, "collapse-toggle").click()
+                menu_button = driver.find_element(By.CLASS_NAME, "collapse-toggle")
+                menu_button.click()
                 logout_button = driver.find_element(By.TEXT_LINK, "Sign out")
                 logout_button.click()
                                                          
