68f48f8c20b9325c42b81f16538090b1f4f44c3d
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-06 18:30:52 -0400

    Update browserstack_techchallenge2.py

--- browserstack_techchallenge2.py
+++ browserstack_techchallenge2.py
@@ -1,117 +1,153 @@
-
-import os
-import json
+from threading import Thread
+from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.safari.options import Options as SafariOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from threading import Thread
+import json
+import os
 
+# Environment Variables
 BS_USERNAME = os.environ.get(
-    "BROWSERSTACK_USERNAME") or "BROWSERSTACK_USERNAME"
+    "BROWSERSTACK_USERNAME")
 BS_ACCESS_KEY = os.environ.get(
-    "BROWSERSTACK_ACCESS_KEY") or "BROWSERSTACK_ACCESS_KEY"
+    "BROWSERSTACK_ACCESS_KEY")
 URL = "https://{}:{}@hub.browserstack.com/wd/hub".format(BS_USERNAME, BS_ACCESS_KEY)
 BS_BUILD_NAME = os.environ.get("BROWSERSTACK_BUILD_NAME")
 
-capabilities = [
+# BrowserStack Trial credentials
+bs_email = os.getenv("BS_Credentials_USR")  # BrowserStack email from Jenkinsfile
+bs_password = os.getenv("BS_Credentials_PSW")  # BrowerStack password from JenkinsFile
+
+# Set up capabilities for each browser
+browsers = [
     {
         "os": "OS X",
-        "osVersion": "Monterey",
-        "build": BS_BUILD_NAME,
-        "sessionName": "BStack parallel python",
-        "browserName": "chrome",
-        "browserVersion": "latest"
+        "osVersion": "Ventura",
+        "sessionName": "BStack parallel python 1",
+        "browserName": "firefox",
+        "browserVersion": "latest",
+        "maskCommands" : "setValues, getValues",
+        "build": BS_BUILD_NAME
     },
     {
         "os": "Windows",
-        "osVersion": "11",
-        "build": BS_BUILD_NAME,
-        "sessionName": "BStack parallel python",
-        "browserName": "firefox",
-        "browserVersion": "latest"
+        "osVersion": "10",
+        "sessionName": "BStack parallel python 2",
+        "browserName": "chrome",
+        "browserVersion": "latest",
+        "maskCommands" : "setValues, getValues",
+        "build": BS_BUILD_NAME
     },
     {
-        "osVersion": "10",
-        "deviceName": "Samsung Galaxy S20",
-        "build": BS_BUILD_NAME,
-        "sessionName": "BStack parallel python",
-        "browserName": "chrome",
-    },
+        "deviceName": "Samsung Galaxy S22",
+        "osVersion": "12.0",
+        "sessionName": "BStack parallel python 3",
+        "browserName": "samsung",
+        "real_mobile": "true",
+        "browserVersion": "latest",
+        "deviceOrientation": "portrait",
+        "maskCommands" : "setValues, getValues",
+        "build": BS_BUILD_NAME
+    }
 ]
 
+# Run function for test 
+def tech_challenge(browser):
+  driver = webdriver.Remote(
+      command_executor=URL,
+      desired_capabilities=browser)
 
-def get_browser_option(browser):
-    switcher = {
-        "chrome": ChromeOptions(),
-        "firefox": FirefoxOptions(),
-        "edge": EdgeOptions(),
-        "safari": SafariOptions(),
-    }
-    return switcher.get(browser, ChromeOptions())
+    # 1. Go to homepage
+  driver.get("https://www.browserstack.com/")
+  driver.maximize_window() # Full width for desktop tests
 
-
-def run_session(cap):
-    bstack_options = {
-        "osVersion": cap["osVersion"],
-        "buildName": cap["buildName"],
-        "sessionName": cap["sessionName"],
-        "userName": BS_USERNAME,
-        "accessKey": BS_ACCESS_KEY,
-        "build": BS_BUILD_NAME,
-    }
-    if "os" in cap:
-        bstack_options["os"] = cap["os"]
-    if "deviceName" in cap:
-        bstack_options['deviceName'] = cap["deviceName"]
-    bstack_options["source"] = "python:sample-main:v1.1"
-    if cap['browserName'] in ['ios']:
-        cap['browserName'] = 'safari'
-    options = get_browser_option(cap["browserName"].lower())
-    if "browserVersion" in cap:
-        options.browser_version = cap["browserVersion"]
-    options.set_capability('bstack:options', bstack_options)
-    if cap['browserName'].lower() == 'samsung':
-        options.set_capability('browserName', 'samsung')
-    driver = webdriver.Remote(
-        command_executor=URL,
-        options=options)
+  try: # Mobile only test
+    mobile_menu = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "primary-menu-toggle"))) # Checks if menu is visible
     try:
-        driver.get("https://bstackdemo.com/")
-        WebDriverWait(driver, 10).until(EC.title_contains("StackDemo"))
-        # Get text of an product - iPhone 12
-        item_on_page = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="1"]/p'))).text
-        # Click the 'Add to cart' button if it is visible
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-            (By.XPATH, '//*[@id="1"]/div[4]'))).click()
-        # Check if the Cart pane is visible
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-            (By.CLASS_NAME, "float-cart__content")))
-        # Get text of product in cart
-        item_in_cart = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-            (By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'))).text
-        # Verify whether the product (iPhone 12) is added to cart
-        if item_on_page == item_in_cart:
-            # Set the status of test as 'passed' or 'failed' based on the condition; if item is added to cart
-            driver.execute_script(
-                'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "iPhone 12 has been successfully added to the cart!"}}')
-    except NoSuchElementException as err:
-        message = "Exception: " + str(err.__class__) + str(err.msg)
-        driver.execute_script(
-            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
-    except Exception as err:
-        message = "Exception: " + str(err.__class__) + str(err.msg)
-        driver.execute_script(
-            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
-    # Stop the driver
+                # Go to login page on mobile
+                mobile_menu.click()
+                login_button = driver.find_element(By.LINK_TEXT, "Sign in")
+                login_button.click()
+                
+                # Login using your trial credentials
+                user_input = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "user_email_login")))
+                user_input.send_keys(bs_email)
+                pass_input = driver.find_element_by_id("user_password")
+                pass_input.send_keys(bs_password)
+                pass_input.send_keys(Keys.RETURN)
+                
+                # 2. Make sure that the homepage includes a link to invite users and retrieve the link’s URL  
+                driver.implicitly_wait(1) # Wait for dashboard/home to load
+                mobile_menu.click()
+                invite_link = find_element(By.LINK_TEXT, "Invite team")
+                invite_page = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "manage-users__invite-copyLink-text")))
+                invite_url = invite_page.get_attribute('innerHTML')
+                print("URL to invite users:", invite_url)
+   
+                # 3. Log out of BrowserStack
+                mobile_menu.click()
+                logout_button = driver.find_element(By.TEXT_LINK, "Sign out")
+                logout_button.click()
+                                                         
+                
+            except NoSuchElementException as err:
+                message = "Exception: " + str(err.__class__) + str(err.msg)
+                driver.execute_script(
+                    'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
+            except Exception as err:
+                message = "Exception: " + str(err.__class__) + str(err.msg)
+                driver.execute_script(
+                    'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
+                
+    # Mark test as passed
+    driver.execute_script(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')          
+            
+  except: # Desktop only test
+    try:
+                # Go to login page on desktop
+                login_button = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.LINK_TEXT, "Sign in")))
+                login_button.click()
+                
+                # Login using your trial credentials
+                user_input = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "user_email_login")))
+                user_input.send_keys(bs_email)
+                pass_input = driver.find_element_by_id("user_password")
+                pass_input.send_keys(bs_password)
+                pass_input.send_keys(Keys.RETURN)
+                
+                # 2. Make sure that the homepage includes a link to invite users and retrieve the link’s URL 
+                invite_link = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.LINK_TEXT, "Invite team")))
+                assert invite_link.is_displayed(), "Invite user link not found on the homepage" # No invite link found in homepage when logged in
+                invite_link.click()
+                invite_page = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "manage-users__invite-copyLink-text")))
+                invite_url = invite_page.get_attribute('innerHTML')
+                print("URL to invite users:", invite_url)
+                
+                # 3. Log out of BrowserStack
+                user_account = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, "account-menu-toggle"))).click() # Wait for the dropdown menu to open
+                logout_button = driver.find_element(By.TEXT_LINK, "Sign out")
+                logout_button.click()
+                                                                                                                
+                
+            except NoSuchElementException as err:
+                message = "Exception: " + str(err.__class__) + str(err.msg)
+                driver.execute_script(
+                    'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
+            except Exception as err:
+                message = "Exception: " + str(err.__class__) + str(err.msg)
+                driver.execute_script(
+                    'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
+                  # Mark test as passed
+    driver.execute_script(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')    
+  finally:
+    # Close the browser
     driver.quit()
-
-
-for cap in capabilities:
-    Thread(target=run_session, args=(cap,)).start()
+            
+for browser in browsers:
+  Thread(target=tech_challenge, args=(browser,)).start()
