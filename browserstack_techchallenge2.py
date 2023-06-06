d8dd7efe4fc5cf149e6138e9177d2aa4beb075fb
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-06 11:50:35 -0400

    Update browserstack_techchallenge2.py

--- browserstack_techchallenge2.py
+++ browserstack_techchallenge2.py
@@ -116,8 +116,7 @@ def tech_challenge(browser):
                 pass_input.send_keys(Keys.RETURN)
                 
                 # 2. Make sure that the homepage includes a link to invite users and retrieve the link’s URL 
-                driver.implicitly_wait(5) # Wait invite page to load
-                invite_link = find_element(By.LINK_TEXT, "Invite team")
+                invite_link = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.LINK_TEXT, "Invite team")))
                 assert invite_link.is_displayed(), "Invite user link not found on the homepage" # No invite link found in homepage when logged in
                 invite_link.click()
                 invite_page = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "manage-users__invite-copyLink-text")))
