4aa2cd22d1667fb07687610643bb09587002af5a
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-06 21:12:25 -0400

    Update browserstack_techchallenge2.py

--- browserstack_techchallenge2.py
+++ browserstack_techchallenge2.py
@@ -93,7 +93,7 @@ def tech_challenge(browser):
    
                 # 3. Log out of BrowserStack
                 driver.find_element(By.ID, "primary-menu-toggle").click()
-                driver.find_element(By.CLASS_NAME, "sign_out_link").click()
+                driver.find_element(By.LINK_TEXT, "Sign out").click()
                 
                 # Mark test as passed
                 driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Test passed"}}') 
