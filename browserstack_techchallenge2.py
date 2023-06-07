1f512c5f4b3584e79227d657d455c9311a6ca0a3
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-07 19:24:37 -0400

    Update browserstack_techchallenge2.py

--- browserstack_techchallenge2.py
+++ browserstack_techchallenge2.py
@@ -58,26 +58,26 @@ def mask_input(input):
 
 # Run function for test 
 def tech_challenge(cap):
-   bstack_options = {
+  bstack_options = {
         "osVersion": cap["osVersion"],
         "build": cap["buildName"],
         "sessionName": cap["sessionName"],
         "userName": BS_USERNAME,
         "accessKey": BS_ACCESS_KEY,
         "maskCommands": "setValues"
-    }
-   if "os" in cap:
+  }
+  if "os" in cap:
         bstack_options["os"] = cap["os"]
-   if "deviceName" in cap:
+  if "deviceName" in cap:
         bstack_options['deviceName'] = cap["deviceName"]
-   bstack_options["source"] = "python:sample-main:v1.1"
-   if cap['browserName'] in ['ios']:
+  bstack_options["source"] = "python:sample-main:v1.1"
+  if cap['browserName'] in ['ios']:
         cap['browserName'] = 'safari'
-   options = get_browser_option(cap["browserName"].lower())
-   if "browserVersion" in cap:
+  options = get_browser_option(cap["browserName"].lower())
+  if "browserVersion" in cap:
         options.browser_version = cap["browserVersion"]
-   options.set_capability('bstack:options', bstack_options)
-   if cap['browserName'].lower() == 'samsung':
+  options.set_capability('bstack:options', bstack_options)
+  if cap['browserName'].lower() == 'samsung':
         options.set_capability('browserName', 'samsung')
    driver = webdriver.Remote(
         command_executor=URL,
