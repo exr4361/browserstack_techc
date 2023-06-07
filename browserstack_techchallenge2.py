17e5479ef392acb33e8f104e92192876eef85318
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-07 04:25:43 -0400

    Update browserstack_techchallenge2.py

--- browserstack_techchallenge2.py
+++ browserstack_techchallenge2.py
@@ -88,7 +88,7 @@ def tech_challenge(browser):
         invite_link.click()
         
         invite_page = driver.find_element_by_class_name("manage-users__invite-copyLink-text")
-        invite_page = find_element_by_xpath('.//span[@class = "manage-users__invite-copyLink-text"]')
+        invite_page = driver.find_element_by_xpath('.//span[@class = "manage-users__invite-copyLink-text"]')
         invite_url = invite_page.text
         print("URL to invite users:", invite_url)
         
